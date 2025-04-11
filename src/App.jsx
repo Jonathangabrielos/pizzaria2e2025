c01ffe89edd00051e6e604e5982727400d83b358
Jonathangabrielos <rm95177@estudante.fieb.edu.br>
2025-04-11 14:08:49 +0000

    imagem poke

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
- 
+import poke from './assets/pokeapi_256.png'
 function App(){
  
   const [nome, setNome] = useState('Ronaldo')
@@ -43,7 +43,9 @@ function App(){
   return (
     <div>
       <h3>Pizzaria 2E</h3>
- 
+
+      <img src={poke} style={{width:330}} />
+
       <Formulario nome="Ricardo"
           idade={45}
           sombra="Digite nome..." />
